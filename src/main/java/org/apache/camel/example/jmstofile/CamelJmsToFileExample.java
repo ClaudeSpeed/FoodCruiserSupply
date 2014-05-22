bb188c3f1d63861bc80cab6f76a78b3b04976777
LukasHaupt <lukas.haupt@gmx.at>
2014-05-22 17:07:42 +0200

    finally get the mail stuff to work!!! if you want to check it out fill in your login in CruiseRouteBuilder. i also managed to receive mails but cant process them right now..

--- src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
+++ src/main/java/org/apache/camel/example/jmstofile/CamelJmsToFileExample.java
@@ -36,47 +36,31 @@ public final class CamelJmsToFileExample {
     }
     
     public static void main(String args[]) throws Exception {
-        // START SNIPPET: e1
+    	
         CamelContext context = new DefaultCamelContext();
-        // END SNIPPET: e1
-        // Set up the ActiveMQ JMS Components
-        // START SNIPPET: e2
+        
+//        from example jmsToFile
         ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost?broker.persistent=false");
-        // Note we can explicit name the component
         context.addComponent("test-jms", JmsComponent.jmsComponentAutoAcknowledge(connectionFactory));
-        // END SNIPPET: e2
-        // Add some configuration by hand ...
-        // START SNIPPET: e3
-        context.addRoutes(new RouteBuilder() {
-            public void configure() {
-                from("test-jms:queue:test.queue").to("file://test");
-                
-            }
-        });
-        System.out.println("blablablala");
-        // END SNIPPET: e3
-        // Camel template - a handy class for kicking off exchanges
-        // START SNIPPET: e4
+        
+        RouteBuilder rb = new CruiseRouteBuilder();
+        
+        context.addRoutes(rb);
+        
         ProducerTemplate template = context.createProducerTemplate();
-        // END SNIPPET: e4
-        // Now everything is set up - lets start the context
+        
         context.start();
-        // Now send some test text to a component - for this case a JMS Queue
-        // The text get converted to JMS messages - and sent to the Queue
-        // test.queue
-        // The file component is listening for messages from the Queue
-        // test.queue, consumes
-        // them and stores them to disk. The content of each file will be the
-        // test we sent here.
-        // The listener on the file component gets notified when new files are
-        // found ... that's it!
-        // START SNIPPET: e5
-        for (int i = 0; i < 10; i++) {
-            template.sendBody("test-jms:queue:test.queue", "Tgzugghughuest Message: " + i);
+        
+        for (int i = 0; i < 2; i++) {
+            template.sendBody("test-jms:queue:test.queue", "Luketest Message: " + i);
         }
-        // END SNIPPET: e5
-        // TEST_COMMIT_ALEX
-        // wait a bit and then stop
+        
+//        private void sendEmail(String body) {
+//            // send the email to your mail server
+//            String url = "smtp://someone@localhost?password=secret&to=incident@mycompany.com";
+//            template.sendBodyAndHeader(url, body, "subject", "New incident reported");
+//        }
+        
         Thread.sleep(1000);
         context.stop();
     }
